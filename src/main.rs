8c05227efde34b5f9b82ec8143d2b6ff3bfa1d9c
TegranGrigorian <tegrgrigor@gmail.com>
2025-05-09 14:44:04 -0400

    first boot and properly printing to screen!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-use core::panic::PanicInfo;
 use uefi::prelude::*;
-use uefi::proto::console::text::Output;
-
+use core::fmt::Write;
 #[entry]
-pub extern "efiapi" fn efi_main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn efi_main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     // Initialize UEFI services
     uefi_services::init(&mut system_table).unwrap_or_default();
     
@@ -16,16 +13,11 @@ pub extern "efiapi" fn efi_main(handle: Handle, mut system_table: SystemTable<Bo
     stdout.clear().unwrap_or_default();
     
     // Display a message
-    writeln!(stdout, "Hello from Woodix OS!").unwrap_or_default();
-    writeln!(stdout, "UEFI application started successfully.").unwrap_or_default();
+    writeln!(stdout, "Your mom").unwrap_or_default();
+    writeln!(stdout, "Woodix").unwrap_or_default();
     
     // Wait for a key press before exiting
     system_table.boot_services().stall(10_000_000); // Stall for 10 seconds
     
     Status::SUCCESS
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
 }
